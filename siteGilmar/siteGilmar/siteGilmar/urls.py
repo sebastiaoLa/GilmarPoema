6bce909024b42c02061c890c5165bc1d3404abe4
sebastiaoLa <sebastiaolacerda@outlook.com>
2017-10-07 14:01:54 -0300

    first commit

---
+++ siteGilmar/siteGilmar/siteGilmar/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for siteGilmar.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', siteGilmar.views.home, name='home'),
+    # url(r'^siteGilmar/', include('siteGilmar.siteGilmar.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
